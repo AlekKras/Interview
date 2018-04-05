77bfb5f22b097bee4a5dc2831df39a39986e6d0e
alekkras <alekforwork@gmail.com>
2018-04-04 20:39:16 -0400

    Foundation

---
+++ Foundation/findLongestWord/1.py
@@ -0,0 +1,33 @@
+import re
+def ask():
+    global S
+    global d
+    S = input("What is your word?\n")
+    d = []
+    def ask_words():
+        try:
+            global num
+            num = int(input("How many words would you like to input?\n"))
+        except ValueError:
+            print("Please, put the integer")
+            ask_words()
+    ask_words()
+    for i in range(0,num):
+        d.append(str(input("Enter the word number " + str(i) + "\n")))
+    print("We are looking for the word " + str(S) +
+          " and the longest occurences of anything in "
+        + str(d) + "\n")
+def subseq(S,d):
+    try:
+        # every word in D is made into a regex pattern interspersed with .* and looked up in S
+        in_dict = {word:len(word)
+                   for word in d
+                   if bool(re.search(pattern=".*".join(word),string=S))}
+        print(max(in_dict, key=in_dict.get))
+    except:
+        print("There is no such subsequence")
+ask()
+subseq(S,d)
+
+
+
---
+++ Foundation/findLongestWord/README.md
@@ -0,0 +1,12 @@
+Given a string <code>S</code> and a set of words <code>D</code>,
+find the longest word in <code>D</code> that is a subsequence of <code>S</code>.
+
+Word W is a subsequence of S if some number of characters, possibly zero, can be deleted from S to form W, without reordering the remaining characters.
+
+Note: D can appear in any format (list, hash table, prefix tree, etc.
+
+For example, given the input of S = "abppplee" and D = {"able", "ale", "apple", "bale", "kangaroo"} the correct output would be "apple"
+
+The words "able" and "ale" are both subsequences of S, but they are shorter than "apple".
+The word "bale" is not a subsequence of S because even though S has all the right letters, they are not in the right order.
+The word "kangaroo" is the longest word in D, but it isn't a subsequence of S.
